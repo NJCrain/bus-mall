79708064631c474a2bbdc083c0d9df31639de4a0
Nicholas Crain <nicholascrain@gmail.com>
2018-09-24 17:30:10 -0700

    adjusted pictureChanger to properly handle not displaying duplicates

--- js/app.js
+++ js/app.js
@@ -1,59 +1,48 @@
 'use strict';
-//add total clicks tracker
+
 var totalClicks = 0;
-
-//make a var for max clicks to make testing/changing the max easier
 var maxClicks = 5;
+var onScreen = document.getElementsByClassName('product');
+var previousAlts = new Array(onScreen.length);
 
-//create an array to store all product objects
 Product.allProducts = [];
 
-var onScreen = document.getElementsByClassName('product');
 
-/*constructor function that creates a product object with the following properties
-    -file path for the image
-    -alt text for the image
-    -total clicks
-    -times shown
-    -was part of previous set or not
-    -stores each prodcut in an array*/
+
+/*constructor function for each product to set its source and alt text and give it properties to track individual clicks and how many times it was displayed*/
 function Product(filePath, altText) {
   this.filePath = filePath;
   this.altText = altText;
   this.clicks = 0;
   this.timesShown = 0;
-  this.wasPrevious = false;
+  this.isDisplayed = false;
   Product.allProducts.push(this);
 }
 
+
+//instantiate all products
 new Product('img/bag.jpg', 'bag');
 new Product('img/banana.jpg', 'banana');
 new Product('img/bathroom.jpg', 'bathroom');
 new Product('img/boots.jpg', 'boots');
 new Product('img/breakfast.jpg', 'breakfast');
 new Product('img/bubblegum.jpg', 'bubblegum');
-// new Product('img/chair.jpg', 'chair');
-// new Product('img/cthulhu.jpg', 'cthulhu');
-// new Product('img/dog-duck.jpg', 'dog-duck');
-// new Product('img/dragon.jpg', 'dragon');
-// new Product('img/pen.jpg', 'pen');
-// new Product('img/pet-sweep.jpg', 'pet-sweep');
-// new Product('img/scissors.jpg', 'scissors');
-// new Product('img/shark.jpg', 'shark');
-// new Product('img/sweep.png', 'sweep');
-// new Product('img/tauntaun.jpg', 'tauntaun');
-// new Product('img/unicorn.jpg', 'unicorn');
-// new Product('img/usb.gif', 'usb');
-// new Product('img/water-can.jpg', 'water-can');
-// new Product('img/wine-glass.jpg', 'wine-glass');
+new Product('img/chair.jpg', 'chair');
+new Product('img/cthulhu.jpg', 'cthulhu');
+new Product('img/dog-duck.jpg', 'dog-duck');
+new Product('img/dragon.jpg', 'dragon');
+new Product('img/pen.jpg', 'pen');
+new Product('img/pet-sweep.jpg', 'pet-sweep');
+new Product('img/scissors.jpg', 'scissors');
+new Product('img/shark.jpg', 'shark');
+new Product('img/sweep.png', 'sweep');
+new Product('img/tauntaun.jpg', 'tauntaun');
+new Product('img/unicorn.jpg', 'unicorn');
+new Product('img/usb.gif', 'usb');
+new Product('img/water-can.jpg', 'water-can');
+new Product('img/wine-glass.jpg', 'wine-glass');
 
-/*event handler for click event
-    -changes prop for all imgs on screen to display they were just shown
-    -adds +1 to seen property for images that were just shown
-    -adds +1 to total clicks
-    -changes each img src to a new, non-duplicate image
-    -changes the alt text for each img as it is added
-    -can remove event listeners if total clicks reaches max?*/
+/*This function will check first is an event is passed (since it is also called to render the pictures the first time). If it was called by an event, then it will increment total clicks and add a click to the specific picture clicked. From there if totalClicks is less than maxClicks it will loop through each image container and  assign a new picture that is currently not displayed and set it's isDisplayed value to true. This also constructs and array of the previously displayed alts to use in comparison to unflag the isDisplayed value for the images that were just shown. If the user has hit the maxNumber of clicks, then the eventlistners will be removed to prevent more clicking causing changes.*/
 function pictureChanger(e) {
   if (e) {
     totalClicks++;
@@ -62,29 +51,50 @@ function pictureChanger(e) {
     for (var i = 0; i < Product.allProducts.length; i++) {
       if (clicked.alt === Product.allProducts[i].altText) {
         Product.allProducts[i].clicks++;
-        Product.allProducts[i].wasPrevious = false;
       }
     }
   }
+
   if (totalClicks < maxClicks) {
     for (i = 0; i < onScreen.length; i++) {
       var random = Math.floor(Math.random() * Product.allProducts.length);
-      while (Product.allProducts[random].wasPrevious === true) {
+      while (Product.allProducts[random].isDisplayed === true) {
         random = Math.floor(Math.random() * Product.allProducts.length);
       }
+      previousAlts[i] = onScreen[i].alt;
       onScreen[i].src = Product.allProducts[random].filePath;
       onScreen[i].alt = Product.allProducts[random].altText;
-      Product.allProducts[random].wasPrevious = true;
+      Product.allProducts[random].isDisplayed = true;
       Product.allProducts[random].timesShown++;
     }
   } else {
     for (i = 0; i < onScreen.length; i++) {
       onScreen[i].removeEventListener('click', pictureChanger);
     }
+    displayData();
+  }
+
+  for (var j = 0; j < Product.allProducts.length; j++) {
+    for (var k = 0; k < previousAlts.length; k++)
+      if (Product.allProducts[j].altText === previousAlts[k]) {
+        Product.allProducts[j].isDisplayed = false;
+      }
   }
 }
 
-//add an event listener for when an image is clicked
+function displayData() {
+  var main = document.querySelector('main');
+  var list = document.createElement('ul');
+  var newLi;
+  for(var i = 0; i < Product.allProducts.length; i++) {
+    newLi = document.createElement('li');
+    newLi.textContent = Product.allProducts[i].altText + ' had ' + Product.allProducts[i].clicks + ' votes out of ' + Product.allProducts[i].timesShown + ' appearances ';
+    list.appendChild(newLi);
+  }
+  main.appendChild(list);
+}
+
+//generate event listeners for all possible image containers
 for (var i = 0; i < onScreen.length; i++) {
   onScreen[i].addEventListener('click', pictureChanger);
   console.log('we are on line 96');
