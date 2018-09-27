85b7313422c171dd0ea334450c0e0ded2f696da6
Nicholas Crain <nicholascrain@gmail.com>
2018-09-27 08:30:23 -0700

    added check for putting products into local storage and storing votes afterfter a full voting cycle

--- js/app.js
+++ js/app.js
@@ -4,10 +4,9 @@ var totalClicks = 0;
 var maxClicks = 25;
 var onScreen = document.getElementsByClassName('product');
 var previousAlts = new Array(onScreen.length);
+var myProducts = localStorage.getItem('products');
 
 Product.allProducts = [];
-
-
 
 /*constructor function for each product to set its source and alt text and give it properties to track individual clicks and how many times it was displayed*/
 function Product(filePath, altText) {
@@ -19,29 +18,35 @@ function Product(filePath, altText) {
   Product.allProducts.push(this);
 }
 
-
 //instantiate all products
-new Product('img/bag.jpg', 'bag');
-new Product('img/banana.jpg', 'banana');
-new Product('img/bathroom.jpg', 'bathroom');
-new Product('img/boots.jpg', 'boots');
-new Product('img/breakfast.jpg', 'breakfast');
-new Product('img/bubblegum.jpg', 'bubblegum');
-new Product('img/chair.jpg', 'chair');
-new Product('img/cthulhu.jpg', 'cthulhu');
-new Product('img/dog-duck.jpg', 'dog-duck');
-new Product('img/dragon.jpg', 'dragon');
-new Product('img/pen.jpg', 'pen');
-new Product('img/pet-sweep.jpg', 'pet-sweep');
-new Product('img/scissors.jpg', 'scissors');
-new Product('img/shark.jpg', 'shark');
-new Product('img/sweep.png', 'sweep');
-new Product('img/tauntaun.jpg', 'tauntaun');
-new Product('img/unicorn.jpg', 'unicorn');
-new Product('img/usb.gif', 'usb');
-new Product('img/water-can.jpg', 'water-can');
-new Product('img/wine-glass.jpg', 'wine-glass');
+if (!myProducts) {
+  new Product('img/bag.jpg', 'bag');
+  new Product('img/banana.jpg', 'banana');
+  new Product('img/bathroom.jpg', 'bathroom');
+  new Product('img/boots.jpg', 'boots');
+  new Product('img/breakfast.jpg', 'breakfast');
+  new Product('img/bubblegum.jpg', 'bubblegum');
+  new Product('img/chair.jpg', 'chair');
+  new Product('img/cthulhu.jpg', 'cthulhu');
+  new Product('img/dog-duck.jpg', 'dog-duck');
+  new Product('img/dragon.jpg', 'dragon');
+  new Product('img/pen.jpg', 'pen');
+  new Product('img/pet-sweep.jpg', 'pet-sweep');
+  new Product('img/scissors.jpg', 'scissors');
+  new Product('img/shark.jpg', 'shark');
+  new Product('img/sweep.png', 'sweep');
+  new Product('img/tauntaun.jpg', 'tauntaun');
+  new Product('img/unicorn.jpg', 'unicorn');
+  new Product('img/usb.gif', 'usb');
+  new Product('img/water-can.jpg', 'water-can');
+  new Product('img/wine-glass.jpg', 'wine-glass');
 
+  localStorage.setItem('products', JSON.stringify(Product.allProducts));
+}
+
+else {
+  Product.allProducts = JSON.parse(myProducts);
+}
 /*This function will check first is an event is passed (since it is also called to render the pictures the first time). If it was called by an event, then it will increment total clicks and add a click to the specific picture clicked. From there if totalClicks is less than maxClicks it will loop through each image container and  assign a new picture that is currently not displayed and set it's isDisplayed value to true. This also constructs and array of the previously displayed alts to use in comparison to unflag the isDisplayed value for the images that were just shown. If the user has hit the maxNumber of clicks, then the eventlistners will be removed to prevent more clicking causing changes.*/
 function pictureChanger(e) {
   if (e) {
@@ -83,28 +88,39 @@ function pictureChanger(e) {
 
 // fills in arrays with data to label and add data to a chart and then draws a horizontal bar chart on the screen
 function displayData() {
-  var ctx  = document.getElementById('data').getContext('2d');
+  var ctx = document.getElementById('data').getContext('2d');
   var labels = [];
   var data = [];
   var colors = [];
 
-  for(var i = 0; i < Product.allProducts.length; i++) {
+  for (var i = 0; i < Product.allProducts.length; i++) {
     labels[i] = Product.allProducts[i].altText;
     data[i] = Product.allProducts[i].clicks;
-    colors[i] = 'rgb(' + Math.floor(Math.random()*256) + ',' + Math.floor(Math.random()*256) + ',' + Math.floor(Math.random()*256) + ')';
+    colors[i] =
+      'rgb(' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ')';
   }
 
   var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
       labels: labels,
-      datasets:[{
-        label: '# of votes',
-        data: data,
-        backgroundColor: colors
-      }]
+      datasets: [
+        {
+          label: '# of votes',
+          data: data,
+          backgroundColor: colors
+        }
+      ]
     }
   });
+
+  localStorage.setItem('products', JSON.stringify(Product.allProducts));
 }
 
 //generate event listeners for all possible image containers
